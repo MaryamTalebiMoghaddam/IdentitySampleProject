427c4ac27903574ca3b82fbebe25b04738ba72bf
MaryamTalebiMoghaddam <122522843+MaryamTalebiMoghaddam@users.noreply.github.com>
2023-05-13 15:57:19 +0430

    Add files to repository

---
+++ IdentityProject/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using IdentityProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityProject.Controllers
+{
+    
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private UserManager<AppUser> UserManager;
+        private SignInManager<AppUser> SignInManager;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            this.UserManager = userManager;
+            this.SignInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Login(string returnUrl)
+        {
+            Login login = new Login();
+            login.ReturnUrl = returnUrl;
+            return View(login);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await UserManager.FindByEmailAsync(login.Email);
+                if (user!=null)
+                {
+                    await SignInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result = await SignInManager.PasswordSignInAsync(user, login.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return Redirect(login.ReturnUrl ?? "/");
+                    }
+                    ModelState.AddModelError(nameof(login.Email), "Login failed!Email or Password is invalid!");
+                }
+            }
+            return View(login);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IdentityProject/Controllers/AdminController.cs
@@ -0,0 +1,133 @@
+﻿using IdentityProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager;
+        private IPasswordHasher<AppUser> passwordHasher;
+        public AdminController(UserManager<AppUser> manager, IPasswordHasher<AppUser> passwordHasher)
+        {
+            userManager = manager;
+            this.passwordHasher = passwordHasher;
+
+        }
+
+        public ViewResult Creat() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser
+                {
+                    UserName = user.Name,
+                    Email = user.Email
+                };
+                IdentityResult result = await userManager.CreateAsync(appUser, user.Password);
+
+                if (result.Succeeded)
+                {
+                    RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+            return View(user);
+        }
+
+        public async Task<IActionResult> Update(string id)
+        {
+           AppUser user = await userManager.FindByIdAsync(id);
+            if (user!=null)
+            {
+                return View(user);
+            }
+            else
+            {
+               return RedirectToAction("Index");
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Update(string id, string email, string password)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                if (!string.IsNullOrEmpty(email))
+                {
+                    user.Email = email;
+                }
+                else
+                    ModelState.AddModelError("", "Email can not be empety");
+                if (!string.IsNullOrEmpty(password))
+                {
+                    user.PasswordHash = passwordHasher.HashPassword(user, password);
+                }
+                else
+                    ModelState.AddModelError("", "Password can not be empety");
+                if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+                {
+                    IdentityResult result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        Errors(result);
+                    }                                           
+                }
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View(user);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View("Index", userManager.Users);
+            if (true)
+            {
+
+            }
+            
+
+        }
+
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
+        }
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+    }
+}
